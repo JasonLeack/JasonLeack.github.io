fdeaeed061200726bc46d34552de4294cfba06cc
Jason Leack <jasonleack@outlook.it>
2021-11-06 12:55:50 +0100

    Sticky header on menu toggle

--- Scripts/StickHeader.js
+++ Scripts/StickHeader.js
@@ -1,17 +1,25 @@
 var $window = $(window);
 
-$(document).on("scroll", 
-	function () 
-	{
-		var $header = document.getElementById('navigation');
-		$header.classList.toggle('sticky', $window.scrollTop()>0);
-	}
-);
+/*Checks if the navigation menu should be sticky or not based on page position*/
+$(document).on("scroll", stickNavigation);
 
+/*Sticks navigation on top of the page when scrolling or when the navigation menu is open*/
+function stickNavigation()
+{
+	var $header = document.getElementById('navigation');
+	var $menuToggle = $('.menuToggle');
+		
+	$header.classList.toggle('sticky', $window.scrollTop()>0 || $menuToggle.hasClass('active'));
+}
+
+/*Toggles the navigation menu*/
 function toggleMenu()
 {
 	var menuToggle=document.querySelector('.menuToggle');
 	var menu=document.querySelector('.navMenu');
+	
 	menuToggle.classList.toggle('active');
 	menu.classList.toggle('active');
+	
+	stickNavigation();
 }
