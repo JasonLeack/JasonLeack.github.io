aaf2fd2080af645bc2893f517acae9f9b5236cb8
Jason Leack <jasonleack@outlook.it>
2021-12-02 22:56:24 +0100

    Save theme preference on reload

---
+++ Scripts/ThemeChange.js
@@ -0,0 +1,53 @@
+window.addEventListener("load", function(){
+    loadThemeSelection();
+});
+
+function loadThemeSelection()
+{
+	if (typeof(Storage) !== "undefined") 
+	{
+		if (sessionStorage.darkTheme) 
+		{ 
+			if (Number(sessionStorage.darkTheme) == 1) 
+			{
+				switchTheme();
+				sessionStorage.darkTheme = 1;
+				
+				document.getElementById("themeCheckbox").checked = false;
+			}
+		}
+	}
+}
+
+function setColors(firstName, secondName)
+{
+	var root = document.querySelector(':root');
+	
+	var t = getComputedStyle(root).getPropertyValue(firstName);
+	
+	root.style.setProperty(firstName, getComputedStyle(root).getPropertyValue(secondName));
+	root.style.setProperty(secondName, t);
+}
+
+function switchTheme()
+{
+	var colors = ['backgroundColor', 'middlegroundColor', 'foregroundColor', 'headerBackgroundColor', 'regularTextColor', 'faintTextColor', 'navLinksColor', 'themeColor'];
+	
+	for (let i = 0; i < colors.length; i++) 
+	{
+		setColors('--' + colors[i], '--' + colors[i] + '_dark');
+	}
+	
+	if (typeof(Storage) !== "undefined") 
+	{
+		if (sessionStorage.darkTheme) 
+		{ 
+			sessionStorage.darkTheme = 1-(Number(sessionStorage.darkTheme));
+		} 
+		else 
+		{
+			sessionStorage.darkTheme = 1;
+		}
+	}
+}
+
--- Scripts/Utilities.js
+++ Scripts/Utilities.js
@@ -3,24 +3,3 @@ function copyToClipboard(text)
 	navigator.clipboard.writeText(text);
 	alert("Copied the text: " + text);
 }
-
-function setColors(firstName, secondName)
-{
-	var root = document.querySelector(':root');
-	
-	var t = getComputedStyle(root).getPropertyValue(firstName);
-	
-	root.style.setProperty(firstName, getComputedStyle(root).getPropertyValue(secondName));
-	root.style.setProperty(secondName, t);
-}
-
-function switchTheme()
-{
-	var colors = ['backgroundColor', 'middlegroundColor', 'foregroundColor', 'headerBackgroundColor', 'regularTextColor', 'faintTextColor', 'navLinksColor', 'themeColor'];
-	
-	for (let i = 0; i < colors.length; i++) 
-	{
-		setColors('--' + colors[i], '--' + colors[i] + '_dark');
-	}
-}
-
