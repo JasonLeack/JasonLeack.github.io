587ad7014dcca9835af8b113254f795f1a9f458c
JasonLeack <jasonleack@outlook.it>
2025-04-16 00:53:00 +0200

    Changes dark theme colors

--- Scripts/ThemeChange.js
+++ Scripts/ThemeChange.js
@@ -1,4 +1,15 @@
-function switchTheme(toggle)
-{
-	document.body.classList.toggle("darkTheme", !toggle.checked);
+function toggleTheme(checkbox){
+	document.body.classList.toggle("darkTheme", !checkbox.checked);
+
+	localStorage.setItem("theme", !checkbox.checked ? "dark" : "light");
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+	const savedTheme = localStorage.getItem("theme");
+	const checkbox = document.getElementById("themeCheckbox");
+  
+	if (savedTheme == "dark") {
+		document.body.classList.add("darkTheme");
+		checkbox.checked = false;
+	}
+});
