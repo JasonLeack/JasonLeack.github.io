f5545f58d34fc27bdd950c3da24dfca662ce2f9b
JasonLeack <jasonleack@outlook.it>
2025-04-16 18:06:52 +0200

    Navigation menu now closes on click

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,4 +1,23 @@
+
+/* --- NAVIGATION --- */
 function toggleNavMenu() {
     const nav = document.getElementById("navigation");
     nav.classList.toggle("show");
 }
+
+function closeNavMenu() {
+    document.getElementById("navigation").classList.remove("show");
+}
+
+function openNavMenu() {
+    document.getElementById("navigation").classList.add("show");
+}
+
+/* Close the menu when the user clicks anything */
+document.addEventListener("click", function(event) {
+    const navigationOptions = document.getElementById("navigationOptions");
+    
+    if (!navigationOptions.contains(event.target)) {
+        closeNavMenu();
+    } 
+});
