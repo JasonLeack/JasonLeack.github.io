94e3204798d3b6b2d073b9a260d6e5b833bec095
JasonLeack <jasonleack@outlook.it>
2025-04-18 14:22:14 +0200

    Added underline for active section

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -21,3 +21,32 @@ document.addEventListener("click", function(event) {
         closeNavMenu();
     } 
 });
+
+
+/* Sets the navigation button as active when in the corresponding section */
+function updateActiveNavLink() {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("#navigation a");
+  
+    let current = "";
+  
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if (scrollY >= sectionTop - 50) {
+            current = section.getAttribute("id");
+        }
+    });
+  
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+};
+
+
+window.addEventListener("load", updateActiveNavLink);
+window.addEventListener("scroll", updateActiveNavLink);
